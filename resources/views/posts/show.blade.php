58a8e2b3baa0c9612a777633b550bfb0ddf34048
kampkelly <kampkellykeys@gmail.com>
2017-06-08 02:17:56 +0100

    second commit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -25,7 +25,8 @@
                                         <div class="row">
                                             <div class="col-sm-1 col-xs-12"></div>
                                             <div class="col-sm-10 col-xs-12">
-                                                 <img src="{{ asset('uploads/'.$post->image) }}" width="100%" height="40%">
+                                               <!--  <img src="{{ asset('uploads/'.$post->image) }}" width="100%" height="40%"> -->
+                                                 <img src="{{ asset('uploads/'.$post->image) }}">
                                             </div>
                                             <div class="col-sm-1 col-xs-12"></div>
                                         </div>
