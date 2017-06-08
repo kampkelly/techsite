f65143a6b30d3fea12fd8096f0c65343f9ac01eb
kampkelly <kampkellykeys@gmail.com>
2017-06-08 02:01:48 +0100

    second commit

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -25,7 +25,7 @@
                                   <h4><a href="post/{{$post->id}}">{{ str_limit($post->title), 50 }}</a></h4>
                                   <h6 class="text-right small">{{$post->created_at->diffForHumans()}} </h6>
                           <div class="panel-headin">
-                                  <img src="http://placehold.it/350x150" width="100%">
+                                  <img src="{{ asset('uploads/'.$post->image) }}" width="100%">
                                   <p class="sp" style="margin-top:0px;">{{ str_limit(strip_tags($post->body), 100) }} <a href="post/{{$post->id}}" class="btn btn-xs btn-primary">Read More...</a> </p>       
                           </div>
                           </div>
@@ -91,7 +91,7 @@
 							<h4><a href="post/{{$post->id}}">{{ str_limit($post->title), 50 }}</a></h4>
 							<h6 class="text-right small">{{$post->created_at}} </h6>
 					<div class="panel-headin">
-							<img src="https://cvws.icloud-content.com/B/AS0v8p0nVP9xE3ESAIPc7540EAg5AX3H3CMGfUQzPXgcmfdBGt372zdP/creative-869200_1920.jpg?o=AtvTt6lcJkUSsGvKYrHddMs4te7f_O_P8x92l8IjZ44M&v=1&x=3&a=BTzroYiS5sBbAzRFOw&e=1495364358&k=SaC-TypsVqUXJYY31bUSMA&fl=&r=71973090-169e-4f01-8049-ddc2b3d7369e-1&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=64&s=J6QB-b58e_Lmb1QkLfeIbcxKEUs&cd=i" width="100%">
+							<img src="{{ asset('uploads/'.$post->image) }}" width="100%">
 							<p class="sp" style="margin-top:0px;">{{ str_limit(strip_tags($post->body), 145) }} <a href="post/{{$post->id}}" class="btn btn-xs btn-primary">Read More...</a> </p>       
 					</div>
 					</div>
