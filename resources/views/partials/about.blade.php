75b50a16f54886ddee4eea3eb059ee4f81062f04
kampkelly <kampkellykeys@gmail.com>
2017-07-18 12:01:48 +0100

    mod

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -4,7 +4,7 @@
              <img src="{{ asset('man.jpg') }}" class="img-responsive img-circle">
          </div>
          <div class="col-ssm-10">
-            <h5>He is a programmer/developer. He makes use of php, laravel, rails, python. He is passionate about making the world a better place.</h5>
+            <h5>He is a programmer/developer who makes use of php, laravel, rails, javascript, etc. Interested in technology, sports, etc.</h5>
          </div>
      </div>
  </div>
