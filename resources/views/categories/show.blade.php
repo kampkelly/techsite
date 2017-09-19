d82ff8ae9e267b96292a017a9c7a90c7ce65ce81
kampkelly <kampkellykeys@gmail.com>
2017-09-19 16:42:34 +0100

    on personal site creation

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,33 +1,39 @@
 @extends('layouts.master')
 
 @section('content')
-  <div class="row">
+   <div class="container">
     <!--INCLUDE SIDEBAR HERE -->
+  <div class="row topspace">
     <div class="col-sm-9">
         <div class="contaier-fluid">
-			<h2 class="text-center" style="text-decoration:underline;">{{$category->name}} Posts</h2>
-			<div style="height:10px" ;></div>
-			@foreach($category->posts as $post)			
-				<div href="/posts/{{$post->id}}" class="category_post col-sm-6 col-md-4 pan_pad">
-					<div class="pane" style="height:360px !important; padding: 15px;">  
-							<h4><a href="/posts/{{$post->id}}">{{$post->title}}</a></h4>
-							<h6 class="text-right small"> {{$post->created_at}}</h6>
+            <h2 class="text-center" style="color: grey;">{{$category->name}}</h2>
+            <div id="other_pos"> 
+			  @foreach($category->posts as $post)	
+				<div href="/post/{{$post->id}}" class="other_post col-sm-6 col-md-4 pan_pad">
+					<div class="pane" style="height:360px !important;">  
+							<h4><a href="/post/{{$post->id}}">{{ str_limit($post->title) }}</a></h4>
+							<h6 class="text-right small">{{$post->created_at->diffForHumans()}}</h6>
 					<div class="panel-headin">
-						<a href="/post/{{$post->id}}">
-							<img src="{{ asset('uploads/'.$post->image) }}" width="100%" class="img-responsiv">
-							<p class="sp" style="margin-top:0px;">{{ str_limit($post->body_text, 145) }}... <a href="/posts/{{$post->id}}" class="btn btn-xs btn-primary">Read More...</a> </p>     
-						</a>  
+            <a href="/post/{{$post->id}}">
+							<img src="{{ asset('uploads/'.$post->image) }}" width="100%">
+							<p class="sp" style="margin-top:0px;">{{ str_limit(strip_tags($post->body), 145) }} <a href="post/{{$post->id}}" class="btn btn-xs btn-primary">Read More...</a> </p>    
+            </a>   
 					</div>
 					</div>
 				</div>
-			@endforeach
-			<div style="height: 40px;"></div>
-			<div class="col-sm-12"> 
+               @endforeach 
+
+            </div>
+              <div class="col-sm-12"> 
+              
                @include('../partials/about')
             </div>
+            <!--ABOUT BEGINS HERE -->
+            <!--ABOUT ENDS HERE -->
         </div>
     </div>
-     @include('../partials/right-sidebar')
+     @include('../partials/newright-sidebar')
 
+</div>
 </div>
 @endsection
