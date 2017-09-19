78af99a1e896fcc60587d912f481398d955bd027
kampkelly <kampkellykeys@gmail.com>
2017-09-19 16:51:03 +0100

    on personal site creation

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-  <div class="container" style="height: 90vh;">
+  <div class="container" style="height: 70vh;">
     <!--INCLUDE SIDEBAR HERE -->
     <div class="topspace">
    <h4>Sorry, the page you are looking for cant be found, please contact the site owner!</h4>
