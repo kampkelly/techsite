5d0342887d77ec959f457794db8d72be90145f34
kampkelly <kampkellykeys@gmail.com>
2017-09-19 16:48:28 +0100

    on personal site creation

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+
+@section('content')
+  <div class="container">
+    <!--INCLUDE SIDEBAR HERE -->
+    <div class="topspace">
+   <h4>Sorry, the page you are looking for cant be found, please contact the site owner!</h4>
+     
+
+</div>
+</div>
+@endsection
