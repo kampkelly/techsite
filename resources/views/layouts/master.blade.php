beafcde283396fb4ee1309111897b14fef69a901
kampkelly <kampkellykeys@gmail.com>
2017-09-19 19:58:55 +0100

    added sitemap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -52,17 +52,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <!--lightbox-->
-        <link rel="stylesheet" type="text/css" media="screen" href="http://cdnjs.cloudflare.com/ajax/libs/fancybox/1.3.4/jquery.fancybox-1.3.4.css" />
-<style type="text/css">
-    a.fancybox img {
-        border: none;
-        box-shadow: 0 1px 7px rgba(0,0,0,0.6);
-        -o-transform: scale(1,1); -ms-transform: scale(1,1); -moz-transform: scale(1,1); -webkit-transform: scale(1,1); transform: scale(1,1); -o-transition: all 0.2s ease-in-out; -ms-transition: all 0.2s ease-in-out; -moz-transition: all 0.2s ease-in-out; -webkit-transition: all 0.2s ease-in-out; transition: all 0.2s ease-in-out;
-    } 
-    a.fancybox:hover img {
-        position: relative; z-index: 999; -o-transform: scale(1.03,1.03); -ms-transform: scale(1.03,1.03); -moz-transform: scale(1.03,1.03); -webkit-transform: scale(1.03,1.03); transform: scale(1.03,1.03);
-    }
-</style>
+       @include('../partials/lightbox') 
         <!--lightbox-->
     </head>
     <body>
@@ -76,20 +66,6 @@
          @include('../partials/footer')
 
          <!--lightbox-->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/fancybox/1.3.4/jquery.fancybox-1.3.4.pack.min.js"></script>
 <script type="text/javascript">
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,10 +12,10 @@
                     <li><a href="#">Terms Of Use</a></li>
                 </ul>
                 <ul class="pull-right" id="right" style="display:inline;">
-                    <li><a href="all_posts.php"><i class="fa fa-google" aria-hidden="true"></i></a></li>
-                    <li><a href="all_posts.php"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                    <li><a href="all_posts.php"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                    <li><a href="all_posts.php"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li style="display: none;"><a href="#" target="blank"><i class="fa fa-google" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.facebook.com/obaro.runor" target="blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="https://twitter.com/kampkelly" target="blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/oghenerunor-adjekpiyede-a03ab9b1/" target="blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                 </ul>
             </div>
             <div class="col-sm-4 col-xs-12" style="padding-left:53px;">
