2ae048473be90c29f705be293ffc184b83acdd9d
kampkelly <kampkellykeys@gmail.com>
2017-09-19 23:57:41 +0100

    add analytics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,14 +49,16 @@
             @foreach($posts as $post)
                 <div href="post/{{$post->id}}" class="other_post col-sm-6 col-md-4 pan_pad">
                     <div class="pane" style="height:360px !important; padding: 15px;">  
-                            <h4><a href="post/{{$post->id}}">{{ str_limit($post->title), 50 }}</a></h4>
+                        <div style="min-height: 21% !important;">
+                            <h4><a href="post/{{$post->id}}">{{ str_limit($post->title, 50) }}</a></h4>
                             <h6 class="text-right small">{{$post->created_at}} </h6>
-                    <div class="panel-headin">
-                <a href="/post/{{$post->id}}">
-                            <img src="{{ asset('uploads/'.$post->image) }}" width="100%">
-                            <p class="sp" style="margin-top:0px;">{{ str_limit(strip_tags($post->body), 145) }} <a href="post/{{$post->id}}" class="btn btn-xs btn-primary">Read More...</a> </p> 
-                </a>      
-                    </div>
+                        </div>
+                        <div class="panel-headin">
+                            <a href="/post/{{$post->id}}">
+                                        <img src="{{ asset('uploads/'.$post->image) }}" width="100%">
+                                        <p class="sp" style="margin-top:0px;">{{ str_limit(strip_tags($post->body), 145) }} <a href="post/{{$post->id}}" class="btn btn-xs btn-primary">Read More...</a> </p> 
+                            </a>      
+                        </div>
                     </div>
                     <div style="height: 40px;"></div>
                 </div>
