a619bcb00772f2ece68708177429a67e77d441dc
kampkelly <kampkellykeys@gmail.com>
2017-09-24 15:06:40 +0100

    changed header background and test new email

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'kampkellykeys@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@theinnovestors.com'),
         'name' => env('MAIL_FROM_NAME', 'mywebsite'),
     ],
 
